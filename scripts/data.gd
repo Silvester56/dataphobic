583a95116a6228b2d8f1107c80a4c894f0226bed
silvester <silvester>
2025-02-22 13:57:41 +0100

    Improve background, fonts, node positioning and create animation on click

--- scripts/data.gd
+++ scripts/data.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
 signal data_erased
+var spriteOpacityPercentage: float = 100
+var spriteOpacityPercentageDelta: float = -2
+var blinking = false
 
+func _process(delta: float) -> void:
+	if blinking:
+		spriteOpacityPercentage = spriteOpacityPercentage + spriteOpacityPercentageDelta
+	if spriteOpacityPercentage == 50:
+		spriteOpacityPercentageDelta = 2
+	elif spriteOpacityPercentage == 100:
+		spriteOpacityPercentageDelta = -2
+	$Area2D/Sprite2D.self_modulate.a = spriteOpacityPercentage / 100
+	
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
-		emit_signal("data_erased")
+		emit_signal("data_erased", event)
 		queue_free()
 
 func setPostionAndScale(x: int, y: int, s: int) -> void:
@@ -12,3 +24,14 @@ func setPostionAndScale(x: int, y: int, s: int) -> void:
 	scale.y = s
 	position.x = x
 	position.y = y
+
+
+func _on_area_2d_mouse_entered() -> void:
+	blinking = true
+
+func _on_area_2d_mouse_exited() -> void:
+	blinking = false
+	spriteOpacityPercentage = 100
+	spriteOpacityPercentageDelta = -2
+	$Area2D/Sprite2D.self_modulate.a = 1
+	
--- scripts/data_manager.gd
+++ scripts/data_manager.gd
@@ -13,19 +13,27 @@ func retrieveData():
 		for j in range(0, sqrt(dataHandlingCapacity)):
 			tmpData = Data.instantiate()
 			tmpData.connect("data_erased", _on_data_erased)
-			tmpData.setPostionAndScale(j * squareSize, i * squareSize, squareSize)
+			tmpData.setPostionAndScale(j * squareSize - 180, i * squareSize - 180, squareSize)
 			add_child(tmpData)
 	dataRemaining = dataHandlingCapacity
 	currentlySearching = false
 	$DataSearchingTimerLabel.visible = false
 
-func _on_data_erased() -> void:
+func _on_data_erased(event) -> void:
 	dataRemaining = dataRemaining - 1
+	$GPUParticles2D.global_position = event.position
+	$GPUParticles2D.emitting = true
 	get_parent().increaseTotalDataErased()
 
 func _ready() -> void:
 	$DataSearchingTimer.wait_time = dataSearchingTime
 	retrieveData()
+
+func roundingTime(time: float) -> String:
+	var result = str(snapped($DataSearchingTimer.time_left, 0.1))
+	if len(result) == 1:
+		result = result + ".0"
+	return result
 
 func _process(delta: float) -> void:
 	if dataRemaining == 0 and !currentlySearching:
@@ -33,7 +41,7 @@ func _process(delta: float) -> void:
 		$DataSearchingTimer.start()
 		$DataSearchingTimerLabel.visible = true
 	if currentlySearching:
-		$DataSearchingTimerLabel.text = str(ceil($DataSearchingTimer.time_left))
+		$DataSearchingTimerLabel.text = roundingTime($DataSearchingTimer.time_left)
 
 func _on_data_searching_timer_timeout() -> void:
 	$DataSearchingTimer.stop()
